e72d3a314fa2f8b358f05d5b19e1324f40205dc8
joshwayman <nowayman121@gmail.com>
2021-03-15 16:47:12 +1100

    Updated what side the scroll is on

--- src/App.js
+++ src/App.js
@@ -12,17 +12,18 @@ function App() {
 
   return (
     <div className="App">
-      <div className="Left">
-      <header className="App-header">
-        <h1>H.F.T. <small>How Freaking Tall?</small></h1>
-        <HeightThing />
-
-        <input type="range" name="heightSlider" min="0" max="1500" onChange={ handleChange } value={height} />
-      </header>
-      </div>
       <div className="Right">
         <TallMachine height={height} />
       </div>
+      <div className="Left">
+        <header className="App-header">
+          <h1>H.F.T. <small>How Freaking Tall?</small></h1>
+          <HeightThing />
+
+          <input type="range" name="heightSlider" min="0" max="1500" onChange={ handleChange } value={height} />
+        </header>
+      </div>
+      
     </div>
   );
 }
